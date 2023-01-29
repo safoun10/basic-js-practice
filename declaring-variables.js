54f1fcea13bea2f9498b1f6be5327374df2ae258
Safoun <safoun.official@gmail.com>
2023-01-29 20:31:08 +0600

    added the fourth section

---
+++ declaring-variables.js
@@ -0,0 +1,19 @@
+/*
+-----------------------------------------------------------------
+How to declare a variable ?
+-----------------------------------------------------------------
+Variable declaration requires five steps :
+
+ 1. type the word "var"
+ 2. name the variable
+ 3. add an equal sign
+ 4. type the value
+ 5. add a semicolon
+
+ An example here is :
+
+ var name = "safoun";
+
+ It is recommended to use proper indentation.
+-----------------------------------------------------------------
+*/
