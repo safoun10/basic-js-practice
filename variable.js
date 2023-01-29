cb0d7277afcc9b811082cdec900e375ae8fbf24d
Safoun <safoun.official@gmail.com>
2023-01-29 20:25:13 +0600

    added third secion

---
+++ variable.js
@@ -0,0 +1,9 @@
+/*
+----------------------------------------------------------------
+What is variable in js ?
+----------------------------------------------------------------
+Variables in js is kind of like a container that holds some data/value . It can possibly change (if you change it). It does vary hence the name is vary + able = variable.
+
+Yeah, that's all I can recall right now .
+----------------------------------------------------------------
+*/
