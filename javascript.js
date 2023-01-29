cb64818b22398488d1488bdf24319f5bdd57845c
Safoun <safoun.official@gmail.com>
2023-01-29 19:53:50 +0600

    added the first file from basic problem solving section

---
+++ javascript.js
@@ -0,0 +1,11 @@
+/*
+------------------------------------------------------------------
+ JavaScript is a scripting/programming language which is basically used for front-end and back-end web development. It is also used in AI platform like chatGPT from OpenAI.
+
+ As the reference from MDN : JavaScript is a scripting or programming language that allows you to implement complex features on web pages — every time a web page does more than just sit there and display static information for you to look at — displaying timely content updates, interactive maps, animated 2D/3D graphics, scrolling video jukeboxes, etc. — you can bet that JavaScript is probably involved.
+
+ As from W3schools : JavaScript is the Programming Language for the Web. JavaScript can update and change both HTML and CSS. JavaScript can calculate, manipulate and validate data.
+
+ freeCodeCamp says : JavaScript is a dynamic programming language that's used for web development, in web applications, for game development, and lots more. It allows you to implement dynamic features on web pages that cannot be done with only HTML and CSS.
+ -----------------------------------------------------------------
+*/
