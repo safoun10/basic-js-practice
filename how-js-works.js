9ed98e4dd0c52750d44af695f2f0ecb21c7883cd
Safoun <safoun.official@gmail.com>
2023-01-29 20:16:23 +0600

    added the second section and solved some in one

---
+++ how-js-works.js
@@ -0,0 +1,5 @@
+/*
+----------------------------------------------------------------------
+As much as my 3 days experience says , js takes everything sequentially , it solves every code one by one . It goes to one line , does what it is commanded to be done or it is suppose to do and then goes to the next line/part of the code. Like every other women , js is never wrong , it's either me or my code , js is always right . But one thing that separates js from other women that js always does what I tell it to do . It never argues with me or anything . I love js.
+----------------------------------------------------------------------
+*/
--- javascript.js
+++ javascript.js
@@ -1,5 +1,7 @@
 /*
 ------------------------------------------------------------------
+What is javascript ?
+------------------------------------------------------------------
  JavaScript is a scripting/programming language which is basically used for front-end and back-end web development. It is also used in AI platform like chatGPT from OpenAI.
 
  As the reference from MDN : JavaScript is a scripting or programming language that allows you to implement complex features on web pages — every time a web page does more than just sit there and display static information for you to look at — displaying timely content updates, interactive maps, animated 2D/3D graphics, scrolling video jukeboxes, etc. — you can bet that JavaScript is probably involved.
