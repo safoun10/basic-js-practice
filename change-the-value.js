f18e50941abf616f90aa0c4f02dd5208d052111c
Safoun <safoun.official@gmail.com>
2023-01-29 20:44:29 +0600

    added the fifth section

---
+++ change-the-value.js
@@ -0,0 +1,10 @@
+/*
+-----------------------------------------------------------------------
+How to change the value of a variable ?
+-----------------------------------------------------------------------
+You can just type another variable with the same name and change the value of it . Since javascript is dynamically typed language , it will take the value from first variable and store it . But, when it reaches the updated variable , it instantly changes the value of that particular variable .
+-----------------------------------------------------------------------
+*/
+
+var age = 10 ;
+age = 17;
