9e95be35280168a7008e34db3f902ae051248e89
Safoun <safoun.official@gmail.com>
2023-01-29 21:02:32 +0600

    added the types of variable section

---
+++ types-of-var.js
@@ -0,0 +1,25 @@
+/*
+--------------------------------------------------------------------
+What are the types of variables ?
+--------------------------------------------------------------------
+There are three types of variables . Which are :
+ 1. string
+ 2. numeric
+ 3. boolean
+
+String type variables usually contains texts and enclosed with one or two quotation marks.
+Frankly speaking , anything that is within double or single quotation marks is considered as a string type variable .
+
+Numeric refers to numbers . Numeric variables doesn't require any quotation marks .
+And you can only assign numbers as a numeric type variable , or else it'll through error .
+
+Boolean type variables can contain only two values . It's either true or false .
+The name "boolean" came from an English Mathematician George Boole in 19th century. 
+
+You'll find all three types of variable down here
+--------------------------------------------------------------------
+*/
+
+var name = "George Boole" ; //string type var
+var from = 1815 ; //numeric type var
+var isDead = true ; //boolean type var
