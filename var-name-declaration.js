df3293de67c5f024ee8e756e0f4d00099069581e
Safoun <safoun.official@gmail.com>
2023-01-29 22:14:48 +0600

    added the naming rules of var file

---
+++ var-name-declaration.js
@@ -0,0 +1,19 @@
+/*
+-----------------------------------------------------------------------------------
+How to name a variable ?
+-----------------------------------------------------------------------------------
+Naming a variable is easy and simple , you just need to follow some simple rules .
+
+Do(s)
+ 1. variable can have any amount of character but short and understandable word is recommended.
+ 2. you can start a variable either with any letter , underscore or dollar sign .
+ 3. any meaningful word consistent with the code is recommended
+ 4. you can use multiple words using camelCase PascalCase or snake_case
+
+Don't(s)
+ 1. any variable can't contain any kind of punctuation marks other than underscore and dollar sign .
+ 2. variable should be and only must be in one word can't contain spaces .
+ 3. you can't start a variable with a number .
+ 4. you can't use any reserved word of javascript such as function/class/var .
+-----------------------------------------------------------------------------------
+*/
